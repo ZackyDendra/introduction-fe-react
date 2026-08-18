89202cd3ea7431d880fac7e3ea40d2ae1e8ca589
Zackydendrap <dendrazacky@gmail.com>
2026-08-18 16:09:07 +0700

    WIP SET UP TAILWIND

--- components/Navigation/NavbarHomepage.jsx
+++ components/Navigation/NavbarHomepage.jsx
@@ -2,11 +2,12 @@ export default function NavbarHomepage() {
   return (
     <>
       <div
-        style={{
-          backgroundColor: "#fa2727",
-          height: "100px",
-          width: "full",
-        }}
+        // style={{
+        //   backgroundColor: "#fa5151",
+        //   height: "100px",
+        //   width: "full",
+        // }}
+        className="bg-amber-900 w-full h-25"
       >
       </div>
     </>
